82ab5478bc0c5fe01662ad9b8eb44891aae0eb66
User <shounbabu31@gmail.com>
2026-03-04 15:07:33 +0600

    update

--- lib/app/app_pages.dart
+++ lib/app/app_pages.dart
@@ -1,6 +1,7 @@
 class AppPages{
   static final String initialPages='/splash_one';
   static final String SplashTwoPages='/splash_two';
-  static final String SplashThreePages='/splash_three';
+  static final String NumberPage='/Sing_in_number_page';
+  static final String SingInPage='/Sing_in_page';
 
 }
