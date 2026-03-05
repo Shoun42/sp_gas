36e9a8d35b77b7abe871324a8b4f1b4ade2c28b9
User <shounbabu31@gmail.com>
2026-03-06 00:54:51 +0600

    update

--- lib/app/app_pages.dart
+++ lib/app/app_pages.dart
@@ -2,6 +2,13 @@ class AppPages{
   static final String initialPages='/splash_one';
   static final String SplashTwoPages='/splash_two';
   static final String NumberPage='/Sing_in_number_page';
-  static final String SingInPage='/Sing_in_page';
+  static final String SingUpPage='/Sing_up_page';
+  static final String verifyPage='/verifyPage';
+  static final String setProfile='/setprofile';
+  static final String SingInPass='/sing_in_Pass';
+  static final String Item_view_page='/itam_view_page';
+  static final String homeView = '/homeView';
+
+  static final String productDetails = '/productsDetails';
 
 }
