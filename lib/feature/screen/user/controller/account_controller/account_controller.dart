745786ef03c39a3e34746a1b5e99efa2ab20c265
User <shounbabu31@gmail.com>
2026-03-06 19:25:04 +0600

    update

---
+++ lib/feature/screen/user/controller/account_controller/account_controller.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:sp_gas/app/app_pages.dart';
+
+class AccountController extends GetxController{
+
+  void moveToEditView(){
+    Get.toNamed(AppPages.editAccount);
+  }
+
+}
