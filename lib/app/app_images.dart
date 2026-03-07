cee1cc2c203cd6ea483a451d1057422002849b91
User <shounbabu31@gmail.com>
2026-03-08 03:48:44 +0600

    update

--- lib/app/app_images.dart
+++ lib/app/app_images.dart
@@ -1,6 +1,8 @@
 class AppImages {
 
-  static final String productImage = "assets/image/Product1.png";
-  static final String Product_details = "assets/image/Product 2.png";
+  static final String productImage = "assets/images/Product1.png";
+  static final String Product_details = "assets/images/Product 2.png";
+  static final String Card = "assets/images/Cart.png";
+  static final String Order = "assets/images/order.png";
 
 }
