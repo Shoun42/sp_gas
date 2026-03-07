88d63b12144b62e85f8876e4cded2bc96b8e60e6
User <shounbabu31@gmail.com>
2026-03-08 03:49:13 +0600

    update

--- lib/app/app_pages.dart
+++ lib/app/app_pages.dart
@@ -1,14 +1,20 @@
 class AppPages{
   static final String initialPages='/splash_one';
-  static final String SplashTwoPages='/splash_two';
-  static final String NumberPage='/Sing_in_number_page';
-  static final String SingUpPage='/Sing_up_page';
+  static final String splashtwopages='/splash_two';
+  static final String numberpage='/Sing_in_number_page';
+  static final String singuppage='/Sing_up_page';
   static final String verifyPage='/verifyPage';
   static final String setProfile='/setprofile';
-  static final String SingInPass='/sing_in_Pass';
-  static final String Item_view_page='/itam_view_page';
+  static final String singinpass='/sing_in_Pass';
+  static final String itemviewpage='/Profile';
   static final String homeView = '/homeView';
-
   static final String productDetails = '/productsDetails';
+  static final String accountView = '/accountView';
+  static final String editAccount = '/editAccount';
+  static final String orderview = '/OrderView';
+  static final String notificationView = '/NotificationView';
+  static final String settingsView = '/SettingsView';
+  static final String monthlyConsumptionView = '/MonthlyConsumptionView';
+
 
 }
