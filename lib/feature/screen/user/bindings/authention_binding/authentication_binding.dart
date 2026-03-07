b0a0210d16e357043b9cfda2edc5e54e2b1ba924
User <shounbabu31@gmail.com>
2026-03-08 03:50:42 +0600

    update

--- lib/feature/screen/user/bindings/authention_binding/authentication_binding.dart
+++ lib/feature/screen/user/bindings/authention_binding/authentication_binding.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
+import 'package:sp_gas/feature/screen/user/controller/authentication_controller/sing_in_controller/sing_in_pass_controller.dart';
+import 'package:sp_gas/feature/screen/user/controller/authentication_controller/sing_up_controller/set_Profile_view_controller.dart';
 import 'package:sp_gas/feature/screen/user/controller/authentication_controller/sing_up_controller/sing_up_controller.dart';
 import 'package:sp_gas/feature/screen/user/controller/authentication_controller/sing_up_controller/sing_up_number_controller.dart' show NumberController;
+import 'package:sp_gas/feature/screen/user/controller/authentication_controller/sing_up_controller/verify_number_view_controller.dart';
 
 class AuthenticationBindings extends Bindings{
   @override
@@ -8,6 +11,9 @@ class AuthenticationBindings extends Bindings{
     // TODO: implement dependencies
     Get.put(NumberController());
     Get.put(SingUpController());
+    Get.put(VerifyNumberViewController());
+    Get.put(SetProfileViewController());
+    Get.put(SingInPassController());
 
   }
 
